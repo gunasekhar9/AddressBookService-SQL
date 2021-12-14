37afd14722b43f1514bd462746f0eadb726418ce
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-14 22:36:39 +0530

    [gunasekhar9] add : AddressBook Service DataBase Table Is Created

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,2 +1,7 @@
 create database AddressBook_Service;
 use AddressBook_Service;
+
+
+create Table address_Book(id int identity (1,1) primary key, Firstname varchar(20), Lastname varchar (20), address varchar(20), City varchar (27), State varchar (15), Zip varchar (6), PhoneNumber varchar(15), Email varchar(35)); 
+exec sp_help address_Book
+select * from address_Book 
